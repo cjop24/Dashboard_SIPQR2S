ff3f56a6d197d680603854f03bd3fb5ff9ac2ada
Carlos Julio Ochoa Pérez <carlos.julio.ochoa.perez@gmail.com>
2026-09-18 12:09:01 -0500

    Zoom mapa

--- app_dashboard.py
+++ app_dashboard.py
@@ -467,7 +467,7 @@ def render_tab_individual(df_base_global, col_mot_esp):
         hover_data={'Cantidad': True, 'lat': False, 'lon': False},
         color='Cantidad',
         color_continuous_scale=px.colors.sequential.Greens,
-        zoom=4.8,  # Aumentado de 4.1 a 4.8 para ver el mapa de Colombia más grande
+        zoom=4.5,  # Aumentado de 4.1 a 4.5 para ver el mapa de Colombia más grande
         center={"lat": 4.5709, "lon": -74.2973},
         map_style="carto-positron"
     )
